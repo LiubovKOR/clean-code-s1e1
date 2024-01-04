79fa24fc737aef88c8fb787674476c1c19ac208a
LiubovKOR <lubovkorotkevich@gmail.com>
2024-01-04 16:10:20 +0300

    refactor: update HTML and CSS according to BEM to rules 1.1 and 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -10,40 +10,48 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("incompleted");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("completed");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add('matrix-item');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add('matrix-item__check-input');
     //label
     var label=document.createElement("label");//label
+    label.classList.add("matrix-item__label");
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add("matrix-item__input");
     //button.edit
     var editButton=document.createElement("button");//edit button
+    editButton.classList.add("matrix-item__edit-btn");
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
+    deleteButton.classList.add("matrix-item__delete-btn");
+
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.classList.add("matrix-item__delete-img");
 
     label.innerText=taskString;
-    label.className='task';
+    //label.className='task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    //editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    //editButton.className="edit";
 
-    deleteButton.className="delete";
+    //deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,8 +92,8 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
+    var editBtn=listItem.querySelector(".matrix-item__edit-btn");
+    var containsClass=listItem.classList.contains("matrix-item--edit");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +107,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    listItem.classList.toggle("matrix-item--edit");
 };
 
 
@@ -154,11 +162,12 @@ addButton.addEventListener("click",ajaxRequest);
 
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
+    console.log(taskListItem);
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
-
+    var editButton=taskListItem.querySelector(".matrix-item__edit-btn");
+    var deleteButton=taskListItem.querySelector(".matrix-item__delete-btn");
+    console.log(editButton);
 
     //Bind editTask to edit button.
     editButton.onclick=editTask;
